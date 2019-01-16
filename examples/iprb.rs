934bf960e7b08ec0ea73910aac5990831548e998
Alan Kavishe <alan@filteris.co.za>
2019-01-16 14:54:30 +0200

    Fix  example

--- examples/iprb.rs
+++ examples/iprb.rs
@@ -21,13 +21,13 @@ fn main() {
     // Step 2: get probabilities of a dominant child from a parent pair
     let dominant_probabilities = parents
         .iter()
-        .map(|&(p1, p2)| p1.has_dominant_child(&p2))
+        .map(|&(p1, p2)| p1.has_dominant_child(p2))
         .collect::<Vec<_>>();
 
     // Step 3: get probabilities of selecting the parent pairs from a population
     let selection_probabilities = parents
         .iter()
-        .map(|&(p1, p2)| population.select_parents(&p1, &p2))
+        .map(|&(p1, p2)| population.select_parents(p1, p2))
         .collect::<Vec<_>>();
 
     // Step 4: zip, fold
